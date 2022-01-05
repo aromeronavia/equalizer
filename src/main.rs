b544c10a77a0315cb8f7adb2e09ef2fef47fccb8
Alberto Romero <aromeronavia@gmail.com>
2022-01-04 20:29:11 -0600

    Add vertical slider

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Scale, Orientation};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.example.HelloWorld")
+        .build();
+
+    app.connect_activate(|app| {
+        let scale = Scale::with_range(Orientation::Vertical, 0.0, 10.0, 2.0);
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Equalizer")
+            .child(&scale)
+            .build();
+
+        window.show();
+    });
+
+    app.run();
 }
